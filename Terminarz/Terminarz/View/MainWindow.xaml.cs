a746b12f662ecdd60d108e783b8e57c18a7c47c4
Marcin Puzoń <marcinpzon@gmail.com>
2025-04-07 19:13:10 +0200

    dodanie kalendarza nawigacyjnego

--- Terminarz/Terminarz/View/MainWindow.xaml.cs
+++ Terminarz/Terminarz/View/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Media;
 using Terminarz.View;
 using Terminarz.ViewModel;
@@ -21,5 +22,27 @@ public partial class MainWindow : Window
     {
         AddEventWindow addEventWindow = new AddEventWindow();
         addEventWindow.ShowDialog();
+    }
+
+    private void CalendarButton_Click(object sender, RoutedEventArgs e)
+    {
+        CalendarPopup.IsOpen = !CalendarPopup.IsOpen;
+    }
+
+    private void MiniCalendar_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+    {
+        if (MiniCalendar.SelectedDate.HasValue)
+        {
+            var selectedDate = MiniCalendar.SelectedDate.Value;
+            CalendarPopup.IsOpen = false;
+
+            if (DataContext is MainViewModel vm)
+            {
+                int delta = DayOfWeek.Monday - selectedDate.DayOfWeek;
+                if (selectedDate.DayOfWeek == DayOfWeek.Sunday) delta = -6;
+                vm.CurrentWeekStart = selectedDate.AddDays(delta);
+                vm.LoadEvents();
+            }
+        }
     }
 }
